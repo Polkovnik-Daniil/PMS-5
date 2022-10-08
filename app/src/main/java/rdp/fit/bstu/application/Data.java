888fe0c92f36943698b11b6a4ef9bc08d81c6a74
Polkovnik-Daniil <0205danik@mail.ru>
2022-10-08 16:47:09 +0300

    Was added Serialise

--- app/src/main/java/rdp/fit/bstu/application/Data.java
+++ app/src/main/java/rdp/fit/bstu/application/Data.java
@@ -2,6 +2,8 @@ package rdp.fit.bstu.application;
 
 import android.media.Image;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -19,34 +21,22 @@ public class Data {
     public String _Category;
     public String _Ingridients;
     public String _Time;
-    public static Data[] JSONDeserialise(String path, String filename) throws IOException, JSONException {
-        File file = new File(path, String filename);
-        BufferedReader bufferedReader;
-        try (FileReader fileReader = new FileReader(file)) {
-            bufferedReader = new BufferedReader(fileReader);
+    public static Data[] JSONDeserialise(String path, String filename) throws IOException {
+        //TODO: Realising Deserialise
+        ObjectMapper mapper = new ObjectMapper();
+        InputStream is = DATA.class.getResourceAsStream(path);
+        if(isFileExists(new File(this.getFilesDir,path))) {
+            DATA data = DATA.getInstance();
+            data = mapper.readValue(is, DATA.class);
+            return data;
         }
-        StringBuilder stringBuilder = new StringBuilder();
-        String line = bufferedReader.readLine();
-        while (line != null){
-            stringBuilder.append(line).append("\n");
-            line = bufferedReader.readLine();
-        }
-        bufferedReader.close();
-        // This responce will have Json Format String
-        String responce = stringBuilder.toString();
-        JSONObject jsonObject  = new JSONObject(responce);
-        Data[] data = jsonObject.get("Datas").toString();
+        return DATA.getInstance();
         return null;
     }
-    public static void JSONSerialise(String path, String filename,Data[] data) throws JSONException, IOException {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("Datas", data);
-        String userString = jsonObject.toString();
-        //!!SET PATH!!
-        File file = new File(path, filename);
-        FileWriter fileWriter = new FileWriter(file);
-        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-        bufferedWriter.write(userString);
-        bufferedWriter.close();
+    public static void JSONSerialise(String path, String filename, Data[] data) throws IOException {
+        //TODO: Realising Serialising
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(new File(path, filename), data);
+        return;
     }
 }
