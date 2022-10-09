819edde6af9b9aee10d57441c63ef49823ea0978
Polkovnik-Daniil <0205danik@mail.ru>
2022-10-09 17:25:07 +0300

    working serialization and deserialization

--- app/src/main/java/rdp/fit/bstu/application/Data.java
+++ app/src/main/java/rdp/fit/bstu/application/Data.java
@@ -1,42 +1,38 @@
 package rdp.fit.bstu.application;
 
-import android.media.Image;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Data {
-    public String _Name;
-    public String _Recipe;
-    public String _Image;
-    public String _Category;
-    public String _Ingridients;
-    public String _Time;
-    public static Data[] JSONDeserialise(String path, String filename) throws IOException {
+    public String _Name = null;
+    public String _Recipe = null;
+    public String _Image = null;
+    public String _Category = null;
+    public String _Ingridients = null;
+    public String _Time = null;
+    public static ArrayList<Data> JSONDeserialise(String path, String filename) throws IOException {
         //TODO: Realising Deserialise
         ObjectMapper mapper = new ObjectMapper();
-        InputStream is = DATA.class.getResourceAsStream(path);
-        if(isFileExists(new File(this.getFilesDir,path))) {
-            DATA data = DATA.getInstance();
-            data = mapper.readValue(is, DATA.class);
+        //InputStream is = Data.class.getResourceAsStream(path + filename);
+        InputStream is = new FileInputStream(path + filename);
+        if(isFileExists(new File(path, filename))) {
+            ArrayList<Data> data = mapper.readValue(is, mapper.getTypeFactory().constructCollectionType(ArrayList.class, Data.class));
             return data;
         }
-        return DATA.getInstance();
         return null;
     }
-    public static void JSONSerialise(String path, String filename, Data[] data) throws IOException {
-        //TODO: Realising Serialising
+    public static void JSONSerialise(String path, String filename, ArrayList<Data> data) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         mapper.writeValue(new File(path, filename), data);
         return;
+    }
+    public static boolean isFileExists(File file) {
+        return file.exists() && !file.isDirectory();
     }
 }
--- app/src/main/java/rdp/fit/bstu/application/MainActivity.java
+++ app/src/main/java/rdp/fit/bstu/application/MainActivity.java
@@ -1,12 +1,12 @@
 package rdp.fit.bstu.application;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
-import org.json.JSONException;
+import android.util.Log;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ArrayList<Data> datas = new ArrayList<Data>();
+        Data data1 = new Data();
+        Data data2 = new Data();
+        data1._Name = "odmvb";
+        data1._Category= "ofdnv";
+        data1._Ingridients="fobn";
+        data1._Recipe="123";
+        data1._Image="f12436obn";
+        data2._Name = "1235";
+        data2._Category= "of1324впdnv";
+        data2._Ingridients="аилошт";
+        data2._Recipe="авим";
+        data2._Image="авмлшот";
+        datas.add(data1);
+        datas.add(data2);
         //!!SET PATH!!
+        Log.d("PATH \t","Path\t"+getApplicationInfo().dataDir + "JSON.json");
         try {
-            Data[] data = Data.JSONDeserialise("/data/app/rdp.fit.bstu.application-EouPMJ3XPB78WGxWoqxR9g==/", "JSON.json");
+            Data.JSONSerialise(getApplicationInfo().dataDir, "JSON.json", datas);
+            ArrayList<Data> data = Data.JSONDeserialise("/data/data/rdp.fit.bstu.application/", "JSON.json");
+            Log.d("","Path\t"+getApplicationInfo().dataDir + "JSON.json");
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
             e.printStackTrace();
         }
     }
